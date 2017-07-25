c5c022b822e2d489004f2a700d36e9841630bf9b
ShailajaV <shailaja.v8@gmail.com>
2017-07-25 14:32:40 -0500

    Added placeholder for advanced search and manuscripts search

--- libapp/classic/src/view/main/Main.js
+++ libapp/classic/src/view/main/Main.js
@@ -52,9 +52,10 @@ Ext.define('library.view.main.Main', {
 					width:400,
 					name: 'searchText',
           padding: '0 0 0 25',
-					bind: {
+          emptyText: 'Search'
+					/*bind: {
 							value: '{searchString}'
-						}
+						}*/
 				},
 				{
 					xtype: 'button',
@@ -80,9 +81,10 @@ Ext.define('library.view.main.Main', {
 					width:400,
 					name: 'searchText',
           padding: '0 0 0 25',
-					bind: {
+          emptyText: 'Search ManuScripts'
+					/*bind: {
 							value: '{searchString}'
-						}
+						}*/
 				},
 				{
 					xtype: 'button',
