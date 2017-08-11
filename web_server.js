0984529d8949aeb4cb5ab770ca79fd7398f30728
dashsant <Santi.Dash@gmail.com>
2017-08-11 11:46:44 -0400

    added code for categorization filter

--- web_server.js
+++ web_server.js
@@ -28,6 +28,43 @@ router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });   
 });
 
+var esFliterObj =
+{
+index: 'nithya_index',
+type: 'manuscript',
+body: 
+{
+  query: { 
+    bool: {
+    must:[
+      {
+       match: {
+       search_all: {
+        query: "???",
+        fuzziness: 1,
+        prefix_length: 1
+      }
+    }}],
+    filter: [ 
+      
+    ]
+    }
+  },
+  aggregations : {
+    category_count : {
+        terms : { 
+            field:"tags"
+        }
+    },
+	script_count : {
+	terms : { 
+		 field:"script"
+		}
+	}
+  }
+}
+}
+; 
 
 var esSearchObj = 
 {
@@ -60,24 +97,57 @@ var esSearchObj =
 }
 // POST method route
 router.post('/librrary/filter', function (req, res) {
-  var sb = esSearchObj;
-  sb.body.query.match.search_all.query = req.body.searchText;
+  var sb;
+  if( req.body.hasOwnProperty("catFilter") || req.body.hasOwnProperty("catFilter")){
+    sb = esFliterObj;
+    sb.body.query.bool.must[0].match.search_all.query  = req.body.searchText;
+    if( req.body.hasOwnProperty("catFilter")){
+      if(req.body.catFilter.length > 0){
+        var catTerms = {};
+        catTerms.terms = { tags:req.body.catFilter}
+        sb.body.query.bool.filter.push(catTerms);
+      }
+    }
+    if( req.body.hasOwnProperty("scriptFilter")){
+      if(req.body.scriptFilter.length > 0){
+        var scriptTerms = {};
+        scriptTerms.terms = { script:req.body.scriptFilter}
+        sb.body.query.bool.filter.push(scriptTerms);
+      }
+    }
+
+  }
+  else{
+    sb = esSearchObj;
+    sb.body.query.match.search_all.query = req.body.searchText;
+  }
   client.search(sb).then(function (resp)
   {
-    var hits = resp.hits.hits;
+    var matchList = processSearchResult( resp.hits , resp.aggregations) 
+    res.setHeader('Content-Type', 'application/json');
+    res.send(matchList)
+  },
+  function (err)
+  {
+    console.trace(err.message);
+  }
+  );
+})
+
+function processSearchResult(hits , aggr)
+{
     var matchList = {
        items:[]
     }
-    hits.forEach(function(el){
+    hits.hits.forEach(function(el){
         var o = {}
-	o.title = el._source.title;
-	o.subject = el._source.subject;
-	o.url = el._source.url;
-	o.script = el._source.script;
-	o.id = el._id;
-	matchList.items.push(o);
-	});
-    var aggr = resp.aggregations;
+        o.title = el._source.title;
+        o.subject = el._source.subject;
+        o.url = el._source.url;
+        o.script = el._source.script;
+        o.id = el._id;
+        matchList.items.push(o);
+        });
     var filterTree = {};
     var b = aggr.script_count.buckets;
     b.forEach(function(el){
@@ -88,20 +158,13 @@ router.post('/librrary/filter', function (req, res) {
         filterTree[el.key] = el.doc_count;
     });
     matchList.filterTree = filterTree;
-    matchList.totalMatched = resp.hits.total;
+    matchList.totalMatched = hits.total;
     if( matchList.totalMatched < 500 )
         matchList.totalReturned = matchList.totalMatched;
     else
-	matchList.totalReturned = 500
-    res.setHeader('Content-Type', 'application/json');
-    res.send(matchList)
-  },
-  function (err)
-  {
-    console.trace(err.message);
-  }
-  );
-})
+        matchList.totalReturned = 500
+    return matchList;
+}
 
 app.use(express.static(path.join(__dirname, 'libapp')));
 
